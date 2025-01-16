5eaef72a05f78e203ca046e38db9a9c6ca3b21ce
afrajwho <afrajrahman@gmail.com>
2025-01-16 19:12:27 +0530

    deploy

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000';
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const USER_API_END_POINT = `${VITE_BACKEND_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${VITE_BACKEND_URL}/api/v1/job`;
